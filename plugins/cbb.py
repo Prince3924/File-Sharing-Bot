ec46e72b0b68cc4d761b034a7b092719a18390c1
Prince3924 <154079395+Prince3924@users.noreply.github.com>
2024-03-12 11:07:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://telegram.me/Jai_Siya_Ram_J'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegram.me/Jai_Siya_Ram_J'>Contact here</a>\n○ Channel : @backup_movies_hub</b>",
+            text = f"<b>○ Creator : <a href='https://telegram.me/Jai_Siya_Ram_J'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegram.me/Jai_Siya_Ram_J'>Contact here</a>\n○ Channel : @backup_movies_hub1</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
